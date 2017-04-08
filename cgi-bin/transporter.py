1b3c90f59dc15a87e428f017633c54901f0a51fb
micklethepickle <michel_ma@hotmail.ca>
2017-04-08 16:29:08 -0400

    test2

--- cgi-bin/transporter.py
+++ cgi-bin/transporter.py
@@ -15,79 +15,10 @@ roomUrl = form["url"].value
 resources_file = open("resources.csv", "r")
 resources = resources_file.read().split(",")
 resources_file.close()
+	
 
-
-elif int(resources[2]) == 1:
-	#regenerate previous room using roomUrl with current gold and mana
-	print "location:{0}\r\n".format(roomUrl)
-elif int(resources[2]) == 0:
-	#room is empty so generate our html
-	resources_file = open("resources.csv", "w")
-	resources_file.write("{0},{1},{2}".format(resources[0]+1, resources[1], '1'))
-	resources_file.close()
-	print "\n\n"
-	print """
-			
-		<!DOCTYPE html>
-		<html>
-		<title>DeuceVille</title>
-		<body style="text-align: center;">
-
-		<h1>Welcome to DeuceVille</h1>
-		<center><img src="http://i.imgur.com/MwyPH84.jpg" alt="DeuceVille" style="width:800px;height:400px;">
-		</center>
-		<form action="http://www.cs.mcgill.ca/~mma106/cgi-bin/room.cgi" method="get">
-		    <input type="text" name="command" placeholder="What will you do at DeuceVille?" style="width:800px;"></br>
-		    <input title="commands: PLAY, DROP, EXIT, REFRESH" style="width:100px
-		      ;height:20px;" type="submit" value="Submit">
-		    <input type="hidden" name="inventory" value="{0},{1}">
-		</form>
-		<center>
-			<table>
-				<tr>
-					<th></th>
-					<th>
-						<form action="http://www.cs.mcgill.ca/~mma106/cgi-bin/transporter.py" method="get">
-							<input type="submit" value="North" />
-							<input type="hidden" name="url" value="http://www.cs.mcgill.ca/~mma106/cgi-bin/room.cgi?command=REFRESH"/>
-							<input type="hidden" name=inventory value="{0},{1}" />
-						</form>
-					</th>
-					<th></th>
-				</tr>
-				<tr>
-					<th>
-						<form action="http://google.com">
-							<input type="submit" value="West" />
-							<input type="hidden" name="url" value="http://www.cs.mcgill.ca/~mma106/cgi-bin/room.cgi?command=REFRESH"/>
-							<input type="hidden" name=inventory value="{0},{1}" />
-						</form>
-					</th>
-					<th></th>
-					<th>
-						<form action="http://google.com">
-							<input type="submit" value="East" />
-							<input type="hidden" name="url" value="http://www.cs.mcgill.ca/~mma106/cgi-bin/room.cgi?command=REFRESH"/>
-							<input type="hidden" name=inventory value="{0},{1}" />
-						</form>
-					</th>
-				</tr>
-				<tr>
-					<th></th>
-					<th>
-						<form action="http://google.com">
-							<input type="submit" value="South" />
-							<input type="hidden" name="url" value="http://www.cs.mcgill.ca/~mma106/cgi-bin/room.cgi?command=REFRESH"/>
-							<input type="hidden" name=inventory value="{0},{1}" />
-						</form>
-					</th>
-					<th></th>
-				</tr>
-			</table>
-		</center>
-
-		</body>
-		</html>
-
-	""".format(mana,gold)
-
+print "\n\n"
+print resources
+print mana
+print gold
+print roomUrl

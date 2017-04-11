337006246556f9fe304dc848ab769ada6787f754
micklethepickle <michel_ma@hotmail.ca>
2017-04-11 13:02:39 -0400

    test2

--- cgi-bin/transportOut.py
+++ cgi-bin/transportOut.py
@@ -7,9 +7,9 @@ resources_file = open("./resources.csv", "r")
 resources = resources_file.read().split(",")
 resources_file.close()
 
-resources_file = open("./resources.csv", "w")
-resources_file.write("{0},{1},{2}".format(int(resources[0]), resources[1], '0'))
-resources_file.close()
+resources_file2 = open("./resources.csv", "w")
+resources_file2.write("{0},{1},{2}".format(int(resources[0]), resources[1], '0'))
+resources_file2.close()
 
 form = cgi.FieldStorage()
 

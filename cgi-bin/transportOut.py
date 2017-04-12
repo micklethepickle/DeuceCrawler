f6df564e79cd4c1a0a9618763868784d8c1c079b
micklethepickle <michel_ma@hotmail.ca>
2017-04-12 15:14:16 -0400

    Test10

--- cgi-bin/transportOut.py
+++ cgi-bin/transportOut.py
@@ -3,23 +3,25 @@ import cgi
 import cgitb
 cgitb.enable()
 
-
-print "Content-type: text/html\n\n"
-
-
 resources_file = open("./resources.csv", "r")
 resources = resources_file.read().split(",")
 resources_file.close()
 
-resources_file2 = open("./resources.csv", "w")
-resources_file2.write("{0},{1},{2}".format(int(resources[0]), resources[1], '0'))
-resources_file2.close()
+resources_file = open("./resources.csv", "w")
+resources_file.write("{0},{1},{2}".format(int(resources[0]), resources[1], '0'))
+resources_file.close()
 
 form = cgi.FieldStorage()
 
+inventory = (form["inventory"].value).split(",")
+mana = inventory[0]
+gold = inventory[1]
+roomUrl = form["URL"].value
+transporterUrl = form["transporter"].value
 
-print form
-print form["inventory"]
-#print "location:{0}\r\n".format(properUrl)
+properUrl = transporterUrl + "?URL=" + roomUrl + "&inventory=" + mana + "%2C" + gold
+
+print "Content-type: text/html"
+print "location:{0}\r\n".format(properUrl)
 
 
